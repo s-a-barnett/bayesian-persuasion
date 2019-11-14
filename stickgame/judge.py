7fad2ecb8a458aa04c5230e8360f159275a17e85
s-a-barnett <sambarnett95@gmail.com>
2019-11-14 10:23:56 -0500

    compute p(long sample) for known biases

--- stickgame/judge.py
+++ stickgame/judge.py
@@ -1,6 +1,42 @@
 from stickgame.irwinhall import IrwinHall
 
-class Judge:
-    def __init__(self, numsticks):
-        self.numsticks = numsticks # Total number of sticks in sample
+class FixedJudge:
+    def __init__(self, numsticks, agent1bias='s', agent2bias='l'):
+        self.numsticks  = numsticks # Total number of sticks in sample
         self.sumlengths = 0.0 # Tracks current sum of stick lengths
+
+        self.agent1obs  = []  # List of moves made by agent 1
+        self.agent2obs  = []  # and agent 2
+
+        self._recompute_sum_and_threshold()
+        self.agent1bias = agent1bias # Determines strategy for agent 1
+        self.agent2bias = agent2bias # and agent 2
+
+        self.upper = 1.0
+        self.lower = 0.0
+
+    # Keeps track of the total stick length needed for the sample to be 'long'
+    def _recompute_sum_and_threshold(self):
+        self.sumlengths = sum(self.agent1obs+self.agent2obs)
+        self.threshold = max(0.5*self.numsticks - self.sumlengths, 0.0)
+
+    # Keeps track of the range of possible future stick lengths
+    def _compute_upper_and_lower(self):
+        for agent in [[self.agent1obs, self.agent1bias], [self.agent2obs, self.agent2bias]]:
+            if agent[0]:
+                if agent[1] == 'l':
+                    self.upper = min(agent[0])
+                elif agent[1] == 's':
+                    self.lower = max(agent[0])
+                elif agent[1] != 'n':
+                    raise ValueError
+
+    def compute_p_long(self):
+        self._recompute_sum_and_threshold()
+        self._compute_upper_and_lower()
+
+        num_unseen = self.numsticks - len(self.agent1obs+self.agent2obs)
+
+        ih = IrwinHall(num_unseen, lower=self.lower, upper=self.upper)
+
+        return 1 - ih.cdf(self.threshold)
--- stickgame/tests/test_judge.py
+++ stickgame/tests/test_judge.py
@@ -1,12 +1,45 @@
 import pytest
 from pytest import approx
-from stickgame.judge import Judge
+import numpy as np
+from stickgame.judge import FixedJudge
 
 @pytest.fixture(params=[3,5])
-def Jdge(request):
-    return Judge(request.param)
+def FJudge(request):
+    return FixedJudge(request.param)
 
-class TestJudge:
-    def test_initialise(self, Jdge):
-        assert isinstance(Jdge.numsticks, int)
-        assert Jdge.sumlengths == approx(0.0)
+class TestFixedJudge:
+    def test_initialise(self, FJudge):
+        assert isinstance(FJudge.numsticks, int)
+        assert FJudge.sumlengths == approx(0.0)
+
+    def test_threshold(self, FJudge):
+        assert FJudge.sumlengths + FJudge.threshold >= 0.5*FJudge.numsticks
+        FJudge.agent1obs.append(np.random.rand())
+        FJudge.agent2obs.append(np.random.rand())
+        FJudge._recompute_sum_and_threshold()
+        assert FJudge.sumlengths + FJudge.threshold >= 0.5*FJudge.numsticks
+
+    def test_lower_upper(self, FJudge):
+        FJudge.agent2obs.append(0.6)
+        FJudge._compute_upper_and_lower()
+        assert FJudge.upper == approx(0.6)
+        FJudge.agent1obs.append(0.41)
+        FJudge._compute_upper_and_lower()
+        assert FJudge.lower == approx(0.41)
+
+    def test_p_long_biased(self, FJudge):
+        assert FJudge.compute_p_long() == approx(0.5)
+        if FJudge.numsticks == 3:
+            FJudge.agent2obs.append(0.6)
+            assert FJudge.compute_p_long() == approx(0.125)
+            FJudge.agent1obs.append(0.41)
+            assert FJudge.compute_p_long() == approx(0.578947)
+
+    def test_p_long_neutral(self, FJudge):
+        FJudge.agent1bias = 'n'
+        FJudge.agent2bias = 'n'
+        if FJudge.numsticks == 3:
+            FJudge.agent2obs.append(0.6)
+            assert FJudge.compute_p_long() == approx(0.595)
+            FJudge.agent1obs.append(0.41)
+            assert FJudge.compute_p_long() == approx(0.51)
