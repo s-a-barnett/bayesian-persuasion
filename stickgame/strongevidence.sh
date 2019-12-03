28bbeaf92166d5cca8d3ed89fb9dc476f4e1adc4
s-a-barnett <sambarnett95@gmail.com>
2019-12-03 16:40:50 -0500

    Scripts written for csv input/output

--- stickgame/strongevidence.sh
+++ stickgame/strongevidence.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Purpose: Read Comma Separated CSV File
 # ------------------------------------------
-INPUT=data/se_parameters.csv
+INPUT=data/test_parameters.csv
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read nSticks agentBias
+while read nSticks agent0stick agent1stick
 do
-	webppl persuasion.wppl --require webppl-csv -- --nSticks $nSticks --agentBiased $agentBiased
+	webppl persuasion.wppl --require webppl-csv -- --nSticks $nSticks --agent0stick $agent0stick --agent1stick $agent1stick
 done < $INPUT
 IFS=$OLDIFS
