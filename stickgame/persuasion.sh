71bb6a9350bb33b80c77a10c9309b3f88d54dec4
s-a-barnett <sambarnett95@gmail.com>
2019-12-04 09:07:15 -0500

    Fixed swapped shell script error

--- stickgame/persuasion.sh
+++ stickgame/persuasion.sh
@@ -5,8 +5,8 @@ INPUT=data/se_parameters.csv
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read nSticks agentBias
+while read nSticks agent0stick agent1stick
 do
-	webppl persuasion.wppl --require webppl-csv -- --nSticks $nSticks --agentBiased $agentBiased
+	webppl persuasion.wppl --require webppl-csv -- --nSticks $nSticks --agent0stick $agent0stick --agent1stick $agent1stick
 done < $INPUT
 IFS=$OLDIFS
--- stickgame/strongevidence.sh
+++ stickgame/strongevidence.sh
@@ -5,8 +5,8 @@ INPUT=data/test_parameters.csv
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read nSticks agent0stick agent1stick
+while read nSticks agentBiased
 do
-	webppl persuasion.wppl --require webppl-csv -- --nSticks $nSticks --agent0stick $agent0stick --agent1stick $agent1stick
+	webppl persuasion.wppl --require webppl-csv -- --nSticks $nSticks --agentBiased $agentBiased
 done < $INPUT
 IFS=$OLDIFS
