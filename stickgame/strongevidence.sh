d5d7866ca6c634ae9a4bbd8ff2b785307ec4199d
s-a-barnett <sambarnett95@gmail.com>
2019-12-04 09:07:43 -0500

    Fixed another shell script error

--- stickgame/strongevidence.sh
+++ stickgame/strongevidence.sh
@@ -7,6 +7,6 @@ IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read nSticks agentBiased
 do
-	webppl persuasion.wppl --require webppl-csv -- --nSticks $nSticks --agentBiased $agentBiased
+	webppl strongevidence.wppl --require webppl-csv -- --nSticks $nSticks --agentBiased $agentBiased
 done < $INPUT
 IFS=$OLDIFS
