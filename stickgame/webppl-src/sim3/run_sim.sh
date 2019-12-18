1df425f738673400d2c28516bc9891715946081c
s-a-barnett <sambarnett95@gmail.com>
2019-12-18 13:43:44 +0000

    Second SEE simulation initial commit w/ renaming

---
+++ stickgame/webppl-src/sim3/run_sim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Purpose: Read Comma Separated CSV File
+# ------------------------------------------
+INPUT=data/test_parameters.csv
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read nSticks agentBiased
+do
+	webppl strongevidence.wppl --require webppl-csv -- --nSticks $nSticks --agentBiased $agentBiased
+done < $INPUT
+IFS=$OLDIFS
