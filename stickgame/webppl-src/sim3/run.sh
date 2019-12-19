ab32467a6f380c80e497f2dede67b4150b615639
s-a-barnett <sambarnett95@gmail.com>
2019-12-19 05:40:25 -0500

    sim3 ready to run

--- stickgame/webppl-src/sim3/run.sh
+++ stickgame/webppl-src/sim3/run.sh
@@ -4,8 +4,11 @@
 #SBATCH --error=output/job_%a.err
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --array=0-63
+#SBATCH --array=13-13
 #SBATCH -t 05:59:59
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-user=samuelab@princeton.edu
 
 # Change to current directory
 cd /home/samuelab/bayesian-persuasion/stickgame/webppl-src/sim3/
