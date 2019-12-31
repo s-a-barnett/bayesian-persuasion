823b18aec7d553aae32314a99911100eb5a27f23
s-a-barnett <sambarnett95@gmail.com>
2019-12-31 06:23:01 -0500

    Results for sim2

--- stickgame/webppl-src/sim2/run1.sh
+++ stickgame/webppl-src/sim2/run1.sh
@@ -4,15 +4,14 @@
 #SBATCH --error=output/job_%a.err
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --array=0-63
-#SBATCH -t 05:59:59
+#SBATCH -t 23:59:59
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=samuelab@princeton.edu
 
 # Change to current directory
 cd /home/samuelab/bayesian-persuasion/stickgame/webppl-src/sim2/
+module load anaconda3
 
 # Execute script
-printf -v numExp "%05d" ${SLURM_ARRAY_TASK_ID}
-srun sh run_sim.sh $numExp
+srun sh run-all.sh
