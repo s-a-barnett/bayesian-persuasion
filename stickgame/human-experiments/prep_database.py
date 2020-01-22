858a59ba87c6c12745e6208bc7f3684f3b1314bd
Robert Hawkins <hawkrobe@gmail.com>
2020-01-21 19:12:59 -0800

    Clean up prep_database.py

--- stickgame/human-experiments/prep_database.py
+++ stickgame/human-experiments/prep_database.py
@@ -8,12 +8,23 @@ user = auth['user']
 pswd = auth['password']
 host = auth['host']
 
-# have to fix this to be able to analyze from local
+# initialize mongo connection
 conn = pm.MongoClient('mongodb://{}:{}@127.0.0.1'.format(user, pswd))
-db = conn['bayesian-persuasion']
-stim_coll = conn['stimuli']
 
-for stick1 in range(.6, 1.1, .1) :
-    for stick2 in range(0, .5, 0.1) :
-        packet = {'stick1' : stick1, 'stick2' : stick2, 'games_already_used' : []}
+# get database for this project
+db = conn['bayesian-persuasion']
+
+# get stimuli collection from this database
+print('possible collections include: ', db.collection_names())
+stim_coll = db['experiment1_stimuli']
+
+# empty stimuli collection if already exists
+# (note this destroys records of previous games)
+if stim_coll.count() != 0 :
+    stim_coll.drop()
+
+# Loop through evidence and insert into collection
+for stick1 in [.6, .7, .8, .9, 1] :
+    for stick2 in [.1, .2, .3, .4, .5] :
+        packet = {'stick1' : stick1, 'stick2' : stick2, 'numGames': 0, 'games' : []}
         stim_coll.insert_one(packet)
