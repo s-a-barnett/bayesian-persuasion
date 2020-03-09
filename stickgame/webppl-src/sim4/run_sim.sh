878615d975726c07bc19f57d7631065e684739c4
s-a-barnett <sambarnett95@gmail.com>
2020-03-09 10:57:40 -0400

    factor plots with explicit bias computed

--- stickgame/webppl-src/sim4/run_sim.sh
+++ stickgame/webppl-src/sim4/run_sim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Purpose: Read Comma Separated CSV File
 # ------------------------------------------
-for number in $(seq 0.025 0.05 1.025)
-do
-echo $number
-webppl see_judgescore.wppl --require ../shared --require webppl-csv -- --stick $number
-done
+# for number in $(seq 0.025 0.05 1.025)
+# do
+# echo $number
+webppl see_judgescore.wppl --require ../shared --require webppl-csv
+# done
