8886491067d0d353b82e3612fa7f46c2a3bc4c5b
s-a-barnett <sambarnett95@gmail.com>
2020-06-25 11:32:59 +0100

    adjusted node usage

--- stickgame/webppl-src/sim6/run-experiment.sh
+++ stickgame/webppl-src/sim6/run-experiment.sh
@@ -4,8 +4,8 @@
 #SBATCH --error=output/job_%a.err
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --array=0-59
-#SBATCH -t 00:30:00
+#SBATCH --array=0-26
+#SBATCH -t 02:00:00
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=samuelab@princeton.edu
@@ -14,8 +14,8 @@
 cd /home/samuelab/bayesian-persuasion/stickgame/webppl-src/sim6/
 module load anaconda3
 
-numStart=$((5 * ${SLURM_ARRAY_TASK_ID}))
-numEnd=$(($numStart + 4))
+numStart=$((12 * ${SLURM_ARRAY_TASK_ID}))
+numEnd=$(($numStart + 11))
 
 # Execute script
 srun sh run-range.sh $numStart $numEnd
