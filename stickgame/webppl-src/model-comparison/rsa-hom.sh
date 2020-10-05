1d2de13f9b6bb285315bc7cbb9456ebe8bbb17f0
s-a-barnett <sambarnett95@gmail.com>
2020-10-05 12:00:53 +0100

    added more arguments, improved recording

--- stickgame/webppl-src/model-comparison/rsa-hom.sh
+++ stickgame/webppl-src/model-comparison/rsa-hom.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-webppl rsa-hom.wppl --require ../shared-simple --require webppl-csv
+while getopts s:b:v: option
+do
+  case "${option}"
+    in
+    s) SAMPLES=${OPTARG};;
+    b) BURN=${OPTARG};;
+    v) VERBOSE=${OPTARG};;
+  esac
+done
+
+webppl rsa-hom.wppl --require ../shared-simple --require webppl-csv \
+  -- --samples $SAMPLES --burn $BURN --verbose $VERBOSE 
