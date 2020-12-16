c146c3e8968486d615a684f3d0a6e5b59fb7faa2
s-a-barnett <sambarnett95@gmail.com>
2020-12-16 09:35:25 +0000

    tidied up shell scripts

--- stickgame/webppl-src/model-comparison/infer-params.sh
+++ stickgame/webppl-src/model-comparison/infer-params.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-while getopts s:b:v:l:o: option
+while getopts m:s:b:v:l:o: option
 do
   case "${option}"
     in
+    m) MODEL=${OPTARG};;
     s) SAMPLES=${OPTARG};;
     b) BURN=${OPTARG};;
     v) VERBOSE=${OPTARG};;
@@ -11,5 +12,5 @@ do
   esac
 done
 
-webppl rsa-het-speakers.wppl --require ../shared-simple --require webppl-csv \
+webppl $MODEL.wppl --require ../shared-simple --require webppl-csv \
   -- --samples $SAMPLES --burn $BURN --verbose $VERBOSE --lag $LAG --out $OUT
