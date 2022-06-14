fbdd0558035ddb2c772eacab6e236819ea554607
Robert Hawkins <hawkrobe@gmail.com>
2022-06-14 01:45:42 -0400

    marginalize over mixture; add offset param

--- models/model-comparison/scripts/model-criterion.py
+++ models/model-comparison/scripts/model-criterion.py
@@ -6,15 +6,14 @@ from scipy.special import logsumexp
 from pathlib import Path
 import os
 import psis
+import math
 
 def compute_mc_stats(d):
     num_obs = len(d.gameid.unique())
     S = len(d) // num_obs
-
     d_numpy = np.stack(d.groupby('gameid')['score'].apply(np.asarray).to_numpy(), axis=1)
     loo, loos, _ = psis.psisloo(d_numpy)
     se_loo = np.sqrt(num_obs * np.var(loos))
-
     lppd_i = d.groupby('gameid').aggregate(logsumexp).to_numpy() - np.log(S)
     pwaic2_i = d.groupby('gameid').var().to_numpy()
     elppd_waic_i = lppd_i - pwaic2_i
@@ -23,7 +22,6 @@ def compute_mc_stats(d):
     pwaic2 = np.sum(pwaic2_i)
     elppd_waic = -2 * (lppd - pwaic2)
     se_elppd_waic = np.sqrt(num_obs * np.var(elppd_waic_i))
-
     out = {
         'loo': -loo,
         'se_loo': se_loo,
@@ -42,13 +40,9 @@ def main(args):
     # models = list(pd.read_csv('./hyperparameters/hyperparameters_waic.csv').model.unique())
     # TODO: base this off of .wppl files
     models = [
-        'aa-hom',
         'aa-hom-adding',
-        'mas-het-simple',
         'mas-het-simple-adding',
-        'mas-het-speakers',
         'mas-het-speakers-adding',
-        'mas-hom',
         'mas-hom-adding',
         'rsa-hom', 
         'rsa-het-simple',
@@ -56,14 +50,23 @@ def main(args):
         'rsa-het-speakers-hi',
     ]
     for model in models:
-        test_path = Path(os.path.join(args.input, f'{model}-pointScores_c0fundefinedoriginal.csv'))
+        test_path = Path(os.path.join(args.input, f'{model}-params-posterior_c0fundefinedreplication.csv'))
         if test_path.is_file():
-            print(f'{model = }')
+            print(f'model = {model}')
             for experiment in ['original', 'replication']:
-                num_chains = 100 if model == 'rsa-hom' else 4
-                dfs = pd.concat([pd.read_csv(os.path.join(args.input, f'{model}-pointScores_c{i}fundefined{experiment}.csv')) for i in range(num_chains)])
+                num_chains = 4
+                dfs = pd.concat([pd.read_csv(os.path.join(args.input, f'{model}-params-posterior_c{i}fundefined{experiment}.csv'),
+                                             on_bad_lines = 'skip') for i in range(num_chains)])
+                mle = np.amax(pd.to_numeric(dfs[dfs.param.str.contains("score")]['val'].to_numpy()))
+                dfs = dfs[dfs.param.str.count("-") == 5].rename(columns={
+                    "param": "gameid", 
+                    "val" : "score"
+                }).reset_index()
+                dfs['counts'] = (dfs.prob * 1000).round()
+                dfs = dfs.loc[dfs.index.repeat(dfs.counts)].drop(['prob','index','counts'], axis=1)
+                dfs['score'] = pd.to_numeric(dfs['score'])
                 stats = compute_mc_stats(dfs)
-                stats.update({'model': model, 'experiment': experiment})
+                stats.update({'model': model, 'experiment': experiment, 'mle': mle})
 
                 df_waic = df_waic.append(stats, ignore_index=True)
 
