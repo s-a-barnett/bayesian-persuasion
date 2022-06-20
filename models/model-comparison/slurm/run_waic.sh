ecc13710d0d8df7c590f20efd6d604d0384e2d08
Robert Hawkins <hawkrobe@gmail.com>
2022-06-20 15:56:13 -0400

    create shell script that runs out of the box

--- models/model-comparison/slurm/run_waic.sh
+++ models/model-comparison/slurm/run_waic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash#
 # parallel --bar --colsep ',' "sh ./run_waic.sh {1} {2} {3} {4} {5} {6} {7}" :::: ../input/hyperparameters.csv
-webppl ./models/$1.wppl --random-seed $6 --require ../shared --require webppl-csv -- --samples $2 --burn $3 --lag $4 --verbose $5 --chain $6 --experiment $7 --out ./output/
+/mnt/cup/people/robertdh/.webppl/webppl ./models/$1.wppl --random-seed $6 --require ../shared --require webppl-csv -- --samples $2 --burn $3 --lag $4 --verbose $5 --chain $6 --experiment $7 --out ./output/
 
